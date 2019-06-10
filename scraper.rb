fe30207d742cee49bef106c406ea4bf0833c246c
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 15:45:10 +1000

    Update to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,3 @@
 require "icon_scraper"
 
-days = ENV['MORPH_DAYS'] || 14
-
-IconScraper.rest_xml("http://rccweb.rockdale.nsw.gov.au/EPlanning/Pages/XC.Track/SearchApplication.aspx", "d=last#{days}days&k=LodgementDate&t=217&o=xml")
+IconScraper.scrape_and_save(:rockdale)
